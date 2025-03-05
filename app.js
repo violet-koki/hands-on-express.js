71a044d3012d8b44cf95d4018fe00f0177f1343f
k.yasumoto <yasumotoke_0808@yahoo.co.jp>
2025-03-05 18:48:30 +0900

    feat: ルーティング、gitigonore追記

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users'); // usersルーターがある場合
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,6 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// // ルーターを使用
+app.use('/', indexRouter);
+app.use('/users', usersRouter); // usersルーターがある場合
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
