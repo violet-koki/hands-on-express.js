e845af4e9517b0d46c9153e41ae22d0a8a483ead
k.yasumoto <yasumotoke_0808@yahoo.co.jp>
2025-03-06 17:27:47 +0900

    feat: タスクは配列に追加する

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+let todos = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'ToDo App',
+    todos: todos,
+  });
 });
+
+router.post('/', function(req, res, next) {
+  const todo = req.body.add;
+  todos.push(todo);
+  // res.render('index', { title: 'Todo App'})
+  res.redirect('/');
+})
 
 module.exports = router;
